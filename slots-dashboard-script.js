54c256ed1efa6ab5278aba4faa88e5a295cac243
Koushik-5499 <koushik4680@gmail.com>
2026-04-05 23:09:10 +0530

    update slots dashboard to match backend parameter names

--- slots-dashboard-script.js
+++ slots-dashboard-script.js
@@ -569,9 +569,9 @@ async function openPayment(bookingId, amount, requestId, requestData) {
                             "Content-Type": "application/json"
                         },
                         body: JSON.stringify({
-                            order_id: response.razorpay_order_id,
-                            payment_id: response.razorpay_payment_id,
-                            signature: response.razorpay_signature
+                            razorpay_order_id: response.razorpay_order_id,
+                            razorpay_payment_id: response.razorpay_payment_id,
+                            razorpay_signature: response.razorpay_signature
                         })
                     });
 
